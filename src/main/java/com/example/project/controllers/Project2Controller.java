2320c3860d07a7382b1b3c1d407025e5a7230f15
George Smokin <lmaosaurus9897@gmail.com>
2023-08-27 14:21:22 +0200

    Portfolio v2 -Added 2 page with 2 project on it -Links on top of website to navigate to project 1 and project 2

--- src/main/java/com/example/project/controllers/Project2Controller.java
+++ src/main/java/com/example/project/controllers/Project2Controller.java
@@ -2,21 +2,17 @@ package com.example.project.controllers;
 
 import com.example.project.models.PhotoDescription;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
-public class MainController {
-    @GetMapping("/")
-    public String homePage(@RequestParam(name = "currentPhotoIndex", defaultValue = "1") int currentPhotoIndex,Model model) {
+public class Project2Controller {
+    @GetMapping("/project2")
+    public String homePage(@RequestParam(name = "currentPhotoIndex", defaultValue = "1") int currentPhotoIndex, Model model) {
 
-        int maxPhotos = 2; // Set your maxPhotos value
+        int maxPhotos = 5; // Set your maxPhotos value
 
         if (currentPhotoIndex < 1) {
             currentPhotoIndex = maxPhotos;
@@ -25,29 +21,25 @@ public class MainController {
         }
 
         // Get photo descriptions
-        String[] photoDescriptions = PhotoDescription.getPr1();
+        String[] photoDescriptions = PhotoDescription.getPr2();
         String currentDescription = photoDescriptions[currentPhotoIndex - 1];
 
-
-        String imagePath = String.format("/images/project1/pr%d.png", currentPhotoIndex);
+        String imagePath = String.format("/images/project2/pr%d.png", currentPhotoIndex);
 
         model.addAttribute("currentPhotoIndex", currentPhotoIndex);
         model.addAttribute("maxPhotos", maxPhotos);
         model.addAttribute("currentDescription", currentDescription);
         model.addAttribute("imagePath", imagePath);
 
-        return "HomePage"; // Return the name of your Thymeleaf template
+        return "Project2"; // Return the name of your Thymeleaf template
     }
-
-
-    @PostMapping("/changePhoto")
+    @PostMapping("/changePhoto2")
     public String changePhoto(
             @RequestParam(name = "currentPhotoIndex") int currentPhotoIndex,
             @RequestParam(name = "direction") int direction,
             Model model) {
-
-        // Calculate new photo index
-        int maxPhotos = PhotoDescription.getPr1().length;
+    // Calculate new photo index
+        int maxPhotos = PhotoDescription.getPr2().length;
         currentPhotoIndex += direction;
         if (currentPhotoIndex < 1) {
             currentPhotoIndex = maxPhotos;
@@ -56,17 +48,17 @@ public class MainController {
         }
 
         // Get photo descriptions
-        String[] photoDescriptions = PhotoDescription.getPr1();
+        String[] photoDescriptions = PhotoDescription.getPr2();
         String currentDescription = photoDescriptions[currentPhotoIndex - 1];
 
         // Construct the image path
-        String imagePath = String.format("/images/project1/pr%d.png", currentPhotoIndex);
+        String imagePath = String.format("/images/project2/pr%d.png", currentPhotoIndex);
 
         model.addAttribute("currentPhotoIndex", currentPhotoIndex);
         model.addAttribute("maxPhotos", maxPhotos);
         model.addAttribute("currentDescription", currentDescription);
         model.addAttribute("imagePath", imagePath);
 
-        return "HomePage"; // Return the name of your Thymeleaf template
+        return "Project2"; // Return the name of your Thymeleaf template
     }
 }
